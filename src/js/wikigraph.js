8d58d2973a4a2a1a721e8feb98b31d8de87878b3
Gabriel Russo <gabrielrcarmo@gmail.com>
2017-03-01 14:19:31 -0300

    melhorando debug

--- src/js/wikigraph.js
+++ src/js/wikigraph.js
@@ -1,6 +1,7 @@
 const wg = require('../../build/Release/wiki-graph-node')
 
 wikiGraph = new wg.WikiGraph('./data/wiki_pages_compressed.txt', './data/wiki_links_compressed.txt')
+// wikiGraph = new wg.WikiGraph('./data/small_pages.txt', './data/small_links.txt')
 console.log("Grafo montado com sucesso!")
 
 module.exports = {
