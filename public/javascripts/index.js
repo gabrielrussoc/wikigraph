65bb05df928de1d13487ccdc1b9c888f628055e9
Gabriel Russo <gabrielrcarmo@gmail.com>
2017-03-01 14:21:21 -0300

    jquery e estilos

---
+++ public/javascripts/index.js
@@ -0,0 +1,9 @@
+$('#to').autocomplete({
+    serviceUrl: '/autocomplete',
+    autoSelectFirst: true
+});
+
+$('#from').autocomplete({
+    serviceUrl: '/autocomplete',
+    autoSelectFirst: true
+});
